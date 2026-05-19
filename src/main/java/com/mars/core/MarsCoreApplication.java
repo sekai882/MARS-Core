9f044cb32f0b5d7168532b050de7b69a0f2d967d
sekai882 <josuemullo10@hotmail.com>
2026-05-19 18:02:23 -0500

    chore: inicializar arquitectura MVC con Spring Boot, Thymeleaf y conexión a PostgreSQL en Supabase

---
+++ src/main/java/com/mars/core/MarsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mars.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mars/core/MarsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
